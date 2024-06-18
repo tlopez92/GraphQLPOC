b503a0666226f6eb80a7f69cfcc8d620fead2f47
TJ Lopez <wtjlopez99@gmail.com>
2024-06-18 16:09:05 -0400

    files

---
+++ Catalog.API/Data/EntityConfigurations/ProductTypeEntityTypeConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ProductType = eShop.Catalog.Models.ProductType;
+
+namespace eShop.Catalog.Data.EntityConfigurations;
+
+internal sealed class ProductTypeEntityTypeConfiguration : IEntityTypeConfiguration<ProductType>
+{
+    public void Configure(EntityTypeBuilder<ProductType> builder)
+    {
+        builder
+            .ToTable("ProductTypes");
+
+        builder
+            .Property(cb => cb.Name)
+            .HasMaxLength(100);
+    }
+}
